a7604250e742337fd0b85f429fe9919f6a16542f
Suresh Kumar Amalapuram <apskumarkrc@gmail.com>
2022-05-01 17:42:54 +0530

    Update preprocess_dataset_ids17.py
    CIL and DIL code changes

--- code/preprocess_dataset_ids17.py
+++ code/preprocess_dataset_ids17.py
@@ -87,3 +87,49 @@ for name in names:
   X_temp = np.array(X_temp)
   #print(X_temp.shape)
   np.save(F"CICIDS2017_Class-wise-normalized_datasets/{name}", X_temp)
+
+
+'''
+names = ['BENIGN.npy','DoSGoldenEye.npy','DoSslowloris.npy',  'Infiltration.npy','WebAttackBruteForce.npy',
+'Bot.npy','DoSHulk.npy', 'FTP-Patator.npy','PortScan.npy', 'WebAttackSqlInjection.npy',
+'DDoS.npy', 'DoSSlowhttptest.npy', 'Heartbleed.npy', 'SSH-Patator.npy', 'WebAttackXSS.npy'
+]
+df = pd.DataFrame()
+for idx,name in enumerate(names):
+  print("loading file:",name)
+  file = np.load(name)
+  temp2 = pd.Series([idx for x in range(file.shape[0])])
+  #file[:,-1] = idx
+  temp = pd.DataFrame(file)
+  temp[' Label'] =temp2# pd.concat([temp, temp2], axis=1)
+  print(temp.head())
+  df = pd.concat([df,temp], axis=0)
+
+print(df.shape)  
+  
+#df[' Label'] = (df[' Label'].apply(lambda x: np.random.randint(15, 20) if x == 0 else x)) 
+print("number of unique labels are",df[' Label'].nunique())
+print("unique labels in total_ds",df[' Label'].unique())
+df2=df 
+unique_label=df2[' Label'].unique()
+print("labels are:",unique_label)
+print("Before removing duplicates",Counter(df2[' Label']))
+print("Total no of rows before removing duplicates",df2.shape[0])
+df2.drop_duplicates(inplace=True)
+print("After removing duplicates",Counter(df2[' Label']))
+print("Total no of rows after removing duplicates",df2.shape[0])
+y = df2.pop(df.columns[-1]).to_frame()
+#for column in df.columns:
+ # df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min() + 1e-5)
+
+multilabel_idx=~df2.duplicated(keep='first')
+   # print("Number of multilabel rows",Counter(multilabel_idx))
+   
+df2=df2[multilabel_idx]
+y=y[multilabel_idx]
+print("Total number of rows after removing multilabel rows",df2.shape[0])
+print("After removing multi_label duplicates",Counter(y[' Label']))
+df2=pd.concat([df2,y],axis=1)    
+for label in unique_label:
+  sub_dataset=df2[df2[' Label']==label]
+  np.save("./CIL/"+str(label)+".npy",sub_dataset)'''
